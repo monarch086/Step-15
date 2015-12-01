6e3b408b7de090492802abf64c8e0bc70af8b9da
monarch086 <sbarsuk88@gmail.com>
2015-12-01 23:39:16 +0200

    Home
    Home

--- Home 1/Home 1.cpp
+++ Home 1/Home 1.cpp
@@ -9,6 +9,7 @@
 
 #include <iostream>
 #include <io.h>
+#include <conio.h>
 #include <string.h>
 
 using namespace std;
@@ -51,7 +52,7 @@ char* prepareString(Language &lang, int index);
 void main()
 {
 	setlocale(LC_ALL, "Ukr");
-	Dictionary Voc;
+	Dictionary Voc = {};
 	initDictionary(Voc);
 	loadWords(Voc, "1.txt");
 	
@@ -87,10 +88,10 @@ void removeDictionary(Dictionary &dictionary)
 {
 	if (dictionary.English.used > 0)
 		for (int i = 0; i < dictionary.English.used; i++)
-			delete[] dictionary.English.words[i].(*ptrans);
+			delete[] dictionary.English.words[i].ptrans;
 	if (dictionary.russian.used > 0)
 		for (int i = 0; i < dictionary.russian.used; i++)
-			delete[] dictionary.russian.words[i].(*ptrans);
+			delete[] dictionary.russian.words[i].ptrans;
 	delete[] dictionary.English.words;
 	delete[] dictionary.russian.words;
 }
@@ -133,12 +134,13 @@ void loadWords(Dictionary &dictionary, char *path)
 			
 			char *space = strchr(buffer, ' ');//находим указатель на пробел между словами
 			
-			int size = space - &buffer;
+			int size = space - &buffer[0];
 			strncpy(Eng, buffer, size);
 			Eng[size] = '\0';
 			
-			size = strlen(buffer) - size - 1;
-			strncpy(rus, *space, size);
+			size = strlen(buffer) - size - 2;
+			space++;//пропускаем пробел
+			strncpy(rus, space, size);
 			rus[size] = '\0';
 			
 			addPairOfWords(dictionary, Eng, rus);
@@ -167,7 +169,7 @@ int addWordToLang(Language &lang, char *word)
 	{
 		if (lang.quantity == lang.used)
 			increaseWordsArray(lang);
-		strcpy(lang.words[lang.used++], word);
+		strcpy(lang.words[lang.used++].word, word);
 		current = lang.used - 1;
 		return current;
 	}
@@ -181,8 +183,8 @@ void setRelations(Dictionary &dictionary, int En, int ru)
 	Word &wEn = dictionary.English.words[En];
 	Word &wRu = dictionary.russian.words[ru];
 	
-	wEn.*ptrans[wEn.quantity - 1] = &wRu;
-	wRu.*ptrans[wRu.quantity - 1] = &wEn;
+	wEn.ptrans[wEn.quantity - 1] = &wRu;
+	wRu.ptrans[wRu.quantity - 1] = &wEn;
 }
 
 void increasePtrArray(Language &lang, int index)
@@ -190,13 +192,13 @@ void increasePtrArray(Language &lang, int index)
 	int &quantity = lang.words[index].quantity;
 	
 	if (quantity == 0)
-		lang.words[index].(*ptrans) = new Word*[++quantity];
+		lang.words[index].ptrans = new Word*[++quantity];
 	else
 	{
-		Word **old = lang.words[index].(*ptrans);
-		lang.words[index].(*ptrans) = new Word*[++quantity];
+		Word **old = lang.words[index].ptrans;
+		lang.words[index].ptrans = new Word*[++quantity];
 		for (int i = 0; i < quantity - 1; i++)
-			lang.words[index].(*ptrans)[i] = *old[i];
+			lang.words[index].ptrans[i] = old[i];
 		delete[] *old;
 	}
 }
@@ -224,18 +226,20 @@ char* translate(Dictionary &dictionary, char *word)
 char* prepareString(Language &lang, int index)
 {
 	char *translation = new char[];
-	translation = lang.words[index].ptrans[0].word;
-	
+	Word w = *lang.words[index].ptrans[0];
+	translation = w.word;
+		
 	if (lang.words[index].quantity > 1)
 	{
-		position = strlen(translation);
+		int position = strlen(translation);
 		translation[position++] = ' ';
 		translation[position++] = '(';
 		
 		for (int i = 1; i < lang.words[index].quantity; i++)
 		{
-			strcpy(translation[position], lang.words[index].ptrans[i].word);
-			position += strlen(lang.words[index].ptrans[i].word);
+			w = *lang.words[index].ptrans[i];
+			strcpy(&translation[position], w.word);
+			position += strlen(w.word);
 			
 			if (i != lang.words[index].quantity - 1)
 			{
